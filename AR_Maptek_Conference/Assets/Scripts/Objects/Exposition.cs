2618897d2977136805e83774a08cb944d928b3bb
Milton Pulgar <miltonmja@hotmail.com>
2019-08-02 18:06:25 -0400

    + Scripts ordenados + script base para charla

---
+++ AR_Maptek_Conference/Assets/Scripts/Objects/Exposition.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Exposition
+{
+    public int date; // dia de la charla
+    public string name_exposition;
+    public string hour; // Horario de la charla. Formato ejemplo "08:00 am - 10:00 am"
+    public int room;
+    public string name_expositor;
+    public Texture photo_expositor;
+    public string info_expositor;
+}
