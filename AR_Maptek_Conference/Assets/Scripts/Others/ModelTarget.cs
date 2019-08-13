3b19bd4ec47f191ebf919650ff2c35095937afbf
Milton Pulgar <miltonmja@hotmail.com>
2019-08-13 17:44:15 -0400

    + Limpieza proyecto: Eliminar scripts, imagenes, archivos del proyecto base "Thropies" + Modificacion en los settings

---
+++ AR_Maptek_Conference/Assets/Scripts/Others/ModelTarget.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class ModelTarget
+{
+    public int Id;
+    public string Name;
+    public GameObject Model;
+    public bool isOverlap;
+    public bool IsActive;
+}
